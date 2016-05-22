eea6175b4f59f3aa83384215a89f9e548ba921c5
Jacopo Di Stanislao <jacopo.distanislao@gmail.com>
2016-05-22 11:01:23 +0200

    reset bob.erl

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -1,15 +1,4 @@
 -module(bob).
 -export([response_for/1]).
 
-response_for(S) ->
-    case res(S) of
-        sure -> "Sure.";
-        chill -> "Whoa, chill out!";
-        _ -> "Whatever."
-    end.
-
-res(S) ->
-    case string:substr(lists:reverse(S), 1, 1) of
-        "?" -> sure;
-        "!" -> chill
-    end.
+response_for(S) -> "Whatever.".
