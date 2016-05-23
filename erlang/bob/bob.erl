98642d6ec50456607e629b141232b228f8490b81
Jacopo Di Stanislao <jacopo.distanislao@yoox.com>
2016-05-23 14:26:32 +0200

    wip bob

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -1,4 +1,24 @@
 -module(bob).
--export([response_for/1]).
+-compile(export_all).
 
-response_for(S) -> "Whatever.".
+response_for(S) ->
+  Checks = [
+    {fun bob:is_empty_string/1, "Fine. Be that way!" },
+    {fun bob:ends_with_question_mark/1, "Sure." },
+    {fun bob:are_capital_letters/1, "Whoa, chill out!" }
+  ],
+  get_response(S, Checks).
+
+get_response(_, []) -> "Whatever.";
+get_response(S, [H|T]) ->
+  {F, M} = H,
+  case F(S) of
+    true -> M;
+    _ -> get_response(S, T)
+  end.
+
+are_capital_letters(S) -> S =:= string:to_upper(S).
+
+ends_with_question_mark(S) -> $? =:= lists:last(S).
+
+is_empty_string(S) -> "" =:= string:strip(S).
