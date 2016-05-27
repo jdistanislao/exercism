b6ff7da0063e05f1d5e2d8ddcd18c1cf989d790d
Jacopo Di Stanislao <jacopo.distanislao@yoox.com>
2016-05-27 15:49:45 +0200

    bob fixed! :)

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -1,36 +1,27 @@
 -module(bob).
 -export([response_for/1]).
 
-response_for(S) ->
+response_for(String) ->
   Checks = [
-    {fun(S) -> says_anything(S) end, "Fine. Be that way!" },
-    {fun(S) -> is_a_question(S) end, "Sure." },
-    {fun(S) -> is_a_shout(S) end, "Whoa, chill out!" }
+    {fun says_anything/1, "Fine. Be that way!" },
+    {fun is_shouting/1,   "Whoa, chill out!" },
+    {fun is_a_question/1, "Sure." },
+    {fun(_) -> true end,  "Whatever." }
   ],
-  get_response(S, Checks).
+  get_response(String, Checks).
 
-get_response(_, []) -> "Whatever.";
-get_response(S, [H|T]) ->
-  {F, M} = H,
-  case F(S) of
-    true -> M;
-    _ -> get_response(S, T)
+get_response(String, [{F, Message}|T]) ->
+  case F(String) of
+    true -> Message;
+       _ -> get_response(String, T)
   end.
 
-is_a_shout(S) ->
-  S =:= string:to_upper(S) andalso lists:any(fun(L) -> is_letter(L) end, S).
+is_shouting(String) ->
+  String =:= string:to_upper(String) andalso
+    lists:any(fun(L) -> L >= $A andalso L =< $Z end, String).
 
-is_a_question(S) ->
-  case is_a_shout(S) of
-    true -> false;
-    _ -> $? =:= lists:last(S)
-  end.
+is_a_question(String) ->
+  $? =:= lists:last(String).
 
-says_anything(S) -> "" =:= string:strip(S).
-
-is_letter(L) when L >= $a andalso L =< $z -> true;
-is_letter(L) when L >= $A andalso L =< $Z -> true;
-is_letter(_) -> false.
-
-
-%% "\n\r \t\v\xA0\x{2002}", "Fine. Be that way!"
+says_anything(String) ->
+  nomatch =/= re:run(String, "^(\\h|\\v)*$", [unicode]).
